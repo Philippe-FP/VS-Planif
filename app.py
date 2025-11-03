2c25624407648d17494c20710bbbf26f9f178ec7
Philippe-FP <134265272+Philippe-FP@users.noreply.github.com>
2025-11-03 18:39:00 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.title("Assistant de planification VS")
+
+st.write("Bonjour 👋")
+st.write("Ceci est une application Streamlit de test.")
+st.write("Elle est hébergée depuis GitHub et affichée via Streamlit Cloud.")
